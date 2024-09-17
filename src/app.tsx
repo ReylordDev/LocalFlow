60d6aba28213a97ef0599dd1bafd74af7f349619
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2024-09-17 20:27:45 +0200

    shadcn? + inter?

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
-    <div>
+    <div className="font-sans bg-background">
       <h1 className="text-4xl text-center text-text">Hello World</h1>
+      <Button variant="default">Click me</Button>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts,tsx}"],
+  darkMode: "selector",
   theme: {
     extend: {
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        foreground: "hsl(var(--foreground))",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
         text: {
           DEFAULT: "var(--text)",
           50: "var(--text-50)",
@@ -75,7 +99,29 @@ module.exports = {
           950: "var(--accent-950)",
         },
       },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  // plugins: [require("tailwindcss-animate")],
 };
